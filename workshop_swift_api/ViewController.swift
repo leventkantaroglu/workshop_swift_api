94090d7a2b63566d7deeb8ebbaf79d4bdea4c68d
Levent KANTAROĞLU <leventkantaroglu@mail.com>
2023-01-29 13:37:19 +0300

    first commit

--- workshop_swift_api/ViewController.swift
+++ workshop_swift_api/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getData()
     }
 
-
+    func getData() {
+        let urlText = "https://jsonplaceholder.typicode.com/todos/1"
+        let url = URL(string: urlText)
+        let session = URLSession.shared
+        var task = session.dataTask(with: url!) { data, _, error in
+            if error != nil {
+                print("Error")
+            } else if data == nil {
+                print("Data null")
+            } else {
+                print(data!)
+            }
+        }
+        task.resume()
+    }
 }
-
